16992c222200ec50949d791422f360dfe15f428c
Hardik Nandlal Lalani <hardik56lalani@gmail.com>
2026-03-03 14:46:14 +0530

    apiresponse and apierror

--- src/index.js
+++ src/index.js
@@ -7,7 +7,14 @@ dotenv.config({
     path:"./env"
 })
 connectDB()
-
+.then(
+    app.listen(process.env.PORT||8000,()=>{
+        console.log(`server is runing at port:${process.env.PORT||8000}`)
+    })
+)
+.catch((error)=>{
+    console.log(`mongodb connection failed${error}`)
+})
 
 
 
