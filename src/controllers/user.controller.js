db620f4b3f10f050ce0e19def677b989e8972623
Hardik Nandlal Lalani <hardik56lalani@gmail.com>
2026-03-08 11:55:18 +0530

    llogic registerr controller

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,6 +1,7 @@
 import {asyncHandler} from "../utils/asynchandler.js"
 import {apiError} from "../utils/apiError.js"
 import {User} from "../models/user.model.js"
+import { apiResponse } from "../utils/apiResponse.js"
 const registerUser =asyncHandler( async (req,res)=>{
      // get user details from frontend
     // validation - not empty
@@ -54,6 +55,16 @@ const registerUser =asyncHandler( async (req,res)=>{
 
 
    })
+
+   const createdUser=User.findById(user._id).select(
+    "-password -refreshToken"
+   )
+   if(!createdUser){
+    throw new apiError(500, "somthing went ")
+   }
+   return res.status(201).json(
+    new apiResponse(200, createdUser,"user registered successfully")
+   )
 
 
 })
